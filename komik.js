51638a366d7c617c1fcc52e5aaa41d2e19c96aed
fathursyh <fathur.syah1@gmail.com>
2024-03-07 12:35:24 +0700

    fix fade in behaviour (once)

--- komik.js
+++ komik.js
@@ -1,7 +1,7 @@
 function inViewport(element) {
   var posisi = element.getBoundingClientRect();
   // cek posisi pas element udah 20% atau 0.2 keliatan
-  return !(posisi.top > innerHeight * 0.8 || posisi.bottom < 0);
+  return !(posisi.top > innerHeight * 0.6 || posisi.bottom < 0);
 }
 
 var myElement = document.querySelectorAll('.komik');
@@ -10,9 +10,6 @@ window.addEventListener("scroll", () => {
     if (inViewport(item)) {
       item.classList.remove("hidden3");
       item.classList.remove("hidden4");
-    } else {
-      item.classList.add("hidden3");
-      item.classList.add("hidden4");
     }
   });
 }, true);
